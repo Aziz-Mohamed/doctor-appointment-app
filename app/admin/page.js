fa15132351fca1125f3d0fe3a8f6d689b8486967
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-11-08 05:50:29 +0200

    Same as last commit but with no error

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,13 +1,13 @@
-"use client";
+
 import React, { useContext } from "react";
 import { AdminAccessProtectionServer } from "@/_components/RoleAccessProtection";
 import  InsertFakeBookingButton  from "@/_components/InsertFakeBookingButton";
-import { AppointmentsContext } from "@/_context/AppointmentsContext";
+// import { AppointmentsContext } from "@/_context/AppointmentsContext";
 // import { useAppointments } from "@/_context/useAppointments";
 
 function page() {
   // const {appointments} = useAppointments();
-  const context = useContext(AppointmentsContext);
+  // const context = useContext(AppointmentsContext);
   return (
     <AdminAccessProtectionServer>
       <div>
@@ -15,7 +15,7 @@ function page() {
 
         <div>
           here is the data fetched
-          <div>{JSON.stringify(appointments)}</div>
+          {/* <div>{JSON.stringify(appointments)}</div> */}
         </div>
 
       </div>
