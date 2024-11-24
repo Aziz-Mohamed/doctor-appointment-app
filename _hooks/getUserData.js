a2a0382404b84898620cf88ba232163052977af8
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-11-24 19:52:46 +0200

    Fix : After merging userUI branch to Main.

---
+++ _hooks/getUserData.js
@@ -0,0 +1,34 @@
+import { createClient as ServerSupabase } from "@/utils/supabase/server";
+import { createClient as ClientSupabase } from "@/utils/supabase/client";
+
+
+
+export async function getUserData(serverOrClient = "server") {
+  const supabase =
+    serverOrClient === "server" ? ServerSupabase() : ClientSupabase();
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data.user) return null;
+  return { data, error };
+}
+
+
+export async function getAdminData(serverOrClient = "server") {
+  const supabase =
+    serverOrClient === "server" ? ServerSupabase() : ClientSupabase();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) return null;
+
+  const userId = data.user.id;
+
+  const { data: roleData, error: roleError } = await supabase
+    .from("user")
+    .select("role")
+    .eq("id", userId)
+    .single();
+
+  if (roleError || roleData?.role !== "admin") return null;
+  return { data, error, roleData, roleError };
+}
+
---
+++ app/login/page.js
@@ -0,0 +1,21 @@
+import GoogleOAuth from "@/_components/GoogleOAuth";
+import Signup from "@/_components/Signup";
+import { loginWithPassword, signupWithPassword } from "./actions";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
+      <Signup />
+      <GoogleOAuth />
+    </div>
+
+    // <form>
+    //   <label htmlFor="email">Email:</label>
+    //   <input id="email" name="email" type="email" required />
+    //   <label htmlFor="password">Password:</label>
+    //   <input id="password" name="password" type="password" required />
+    //   <button formAction={loginWithPassword}>Log in</button>
+    //   <button formAction={signupWithPassword}>Sign up</button>
+    // </form>
+  );
+}
